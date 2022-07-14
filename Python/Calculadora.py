6b98808372f3495ec8b2066cae58b55b17e948fe
PedroMoreira83 <109294220+PedroMoreira83@users.noreply.github.com>
2022-07-14 14:17:38 -0300

    calculadora

---
+++ Python/Calculadora.py
@@ -0,0 +1,12 @@
+def calculadora(n1,n2):
+    soma = n1 + n2
+    sub = n1 - n2
+    multi = n1 % n2
+    divi = n1 / n2
+    return (soma, sub, multi, divi)
+n1 = 2
+n2 = 10
+print(calculadora(n1,n2))
+
+
+
