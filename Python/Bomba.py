43d4b89b8837644c11ec3110926f9f64f87977fc
PedroMoreira83 <109294220+PedroMoreira83@users.noreply.github.com>
2022-07-14 13:12:09 -0300

    Formacao Acelerada Softex

---
+++ Python/Bomba.py
@@ -0,0 +1,12 @@
+import time
+
+print('----------------------------------------')
+print(' Iniciando contagem regressiva da bomba ')
+print('----------------------------------------')
+
+for bomba in range(10, -1, -1):
+    print(bomba)
+    time.sleep(1)
+    print('')
+
+print('BOOOOOOOMMMMMMM')
---
+++ Python/TDA-NumerosComplexos.py
@@ -0,0 +1,13 @@
+x=complex(30,20)
+print(x)
+y=complex(50,40)
+print (y)
+z=x+y
+print(z)
+z=x*y
+print(z)
+x=x/y
+print(z)
+print(x.imag)
+print(x.real)
+print(x.conjugate())
