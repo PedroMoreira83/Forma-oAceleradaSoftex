508399c874d4cc4c193200756678528e24ceab64
PedroMoreira83 <109294220+PedroMoreira83@users.noreply.github.com>
2022-08-19 14:30:27 -0300

    New

---
+++ Softex - Formação Acelerada/VSCODE/Softex/configIDE.js
@@ -0,0 +1,11 @@
+/* Optei pelo VSCode pela versatilidade dele. 
+Para fazer funcionar para JavaScript, instalei as seguintes extensões:
+
+- Code Runner;
+- ESLint;
+- Node.js Extension Pack;
+- npm intellisense;
+- Live Server (para testar o front);
+- Prittier (customização). */
+
+console.log('Hello, World!');
