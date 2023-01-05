2622f7aeea1a99176fb310ec6ea8d466577f5005
PedroMoreira83 <109294220+PedroMoreira83@users.noreply.github.com>
2023-01-05 16:33:21 -0300

    Update servidor.js

--- Softex - Formação Acelerada/VSCODE/Softex/servidor.js
+++ Softex - Formação Acelerada/VSCODE/Softex/servidor.js
@@ -1,11 +1,23 @@
 const http = require('http');
+const url = require('url');
+const PORTA = process.PORT || 8080;
 
-const PORTA = process.PORT || 8888;
+const servidor = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  if (req.url == '/') {
+    res.write('<h1>Olá, Mundo!</h1>');
+  } else if (req.url == '/teste1') {
+    res.write('<h1>Teste 1</h1>');
+  }
+  if (req.url == '/teste2') {
+    res.write('<h1>Teste 2</h1>');
+  }
 
-const servidor = http.createServer();
+  const q = url.parse(req.url, true).query;
+  res.write('<br/>' + q.nome);
+  res.write('<br/>' + q.apelido);
 
-servidor.on('request', (request, response) => {
-  response.end('Olá, Mundo!');
+  res.end();
 });
 
 servidor.listen(PORTA, () => {
